ef9ff1eef7b553dfb12606eb6fa596340ed4e6ef
MelnykovMykyta <melnikovnikita@ukr.net>
2024-03-19 03:42:24 +0200

    add actions (model, viewModel, tableview, TVC) +firebase (icon, json)

---
+++ Coffeee/Models/Action.swift
@@ -0,0 +1,8 @@
+//
+//  Action.swift
+//  Coffeee
+//
+//  Created by Nikita Melnikov on 19.03.2024.
+//
+
+import Foundation
---
+++ Coffeee/ViewModels/MainViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  MainViewModel.swift
+//  Coffeee
+//
+//  Created by Nikita Melnikov on 19.03.2024.
+//
+
+import Foundation
